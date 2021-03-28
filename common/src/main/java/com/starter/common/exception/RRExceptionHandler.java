8ab769d4a6ab1b9bd06fb5d62059a42e2e7dd21c
master <master@putaoduo.com>
2021-03-28 17:34:46 +0800

    ig

--- common/src/main/java/com/starter/common/exception/RRExceptionHandler.java
+++ common/src/main/java/com/starter/common/exception/RRExceptionHandler.java
@@ -1,14 +1,15 @@
 /**
  * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
+ * <p>
  * https://www.renren.io
- *
+ * <p>
  * 版权所有，侵权必究！
  */
 
 package com.starter.common.exception;
 
 //import org.apache.shiro.authz.AuthorizationException;
+
 import com.starter.common.utils.R;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,31 +25,31 @@ import org.springframework.web.servlet.NoHandlerFoundException;
  */
 @RestControllerAdvice
 public class RRExceptionHandler {
-	private Logger logger = LoggerFactory.getLogger(getClass());
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
-	/**
-	 * 处理自定义异常
-	 */
-	@ExceptionHandler(RRException.class)
-	public R handleRRException(RRException e){
-		R r = new R();
-		r.put("code", e.getCode());
-		r.put("msg", e.getMessage());
+    /**
+     * 处理自定义异常
+     */
+    @ExceptionHandler(RRException.class)
+    public R handleRRException(RRException e) {
+        R r = new R();
+        r.put("code", e.getCode());
+        r.put("msg", e.getMessage());
 
-		return r;
-	}
+        return r;
+    }
 
-	@ExceptionHandler(NoHandlerFoundException.class)
-	public R handlerNoFoundException(Exception e) {
-		logger.error(e.getMessage(), e);
-		return R.error(404, "路径不存在，请检查路径是否正确");
-	}
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public R handlerNoFoundException(Exception e) {
+        logger.error(e.getMessage(), e);
+        return R.error(404, "路径不存在，请检查路径是否正确");
+    }
 
-	@ExceptionHandler(DuplicateKeyException.class)
-	public R handleDuplicateKeyException(DuplicateKeyException e){
-		logger.error(e.getMessage(), e);
-		return R.error("数据库中已存在该记录");
-	}
+    @ExceptionHandler(DuplicateKeyException.class)
+    public R handleDuplicateKeyException(DuplicateKeyException e) {
+        logger.error(e.getMessage(), e);
+        return R.error("数据库中已存在该记录");
+    }
 
 //	@ExceptionHandler(AuthorizationException.class)
 //	public R handleAuthorizationException(AuthorizationException e){
