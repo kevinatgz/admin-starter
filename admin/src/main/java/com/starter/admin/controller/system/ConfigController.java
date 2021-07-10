7ad40cffb54f7cd7a90da6f0d80fdfdfda2d3ff2
kelven <kevinatgz@qq.com>
2021-07-10 11:31:45 +0800

    consul

---
+++ admin/src/main/java/com/starter/admin/controller/system/ConfigController.java
@@ -0,0 +1,34 @@
+package com.starter.admin.controller.system;
+
+
+
+
+
+import com.starter.admin.config.MySQLProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ConfigController {
+
+    @Autowired
+    private MySQLProperties mySQLProperties;
+
+    @Value("${name}")
+    private String name;
+
+    @GetMapping("/name")
+    public String getName() {
+        return name;
+    }
+
+    @GetMapping("/mysql")
+    public MySQLProperties getMySQLProperties() {
+        return mySQLProperties;
+    }
+
+}
